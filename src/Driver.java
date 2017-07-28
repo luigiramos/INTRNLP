9160e02cddb99cd8faf945e1bdc43cd62f668699
G <luigiramos166@gmail.com>
2017-07-28 17:14:33 +0800

    Parse .csv + Standard Control
    testing lang action, ignore "Action:" outputs

---
+++ src/Driver.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		IE ie = new IE();
+		try {
+			ie.Run();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
